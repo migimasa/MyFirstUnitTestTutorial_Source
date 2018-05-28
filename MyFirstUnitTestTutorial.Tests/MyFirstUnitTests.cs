1f6e907f8464f51002b15dba51e2586a03033c2e
Unknown <migimasa@gmail.com>
2018-05-28 18:34:12 -0500

    Initial Commit

---
+++ MyFirstUnitTestTutorial.Tests/MyFirstUnitTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstUnitTestTutorial.Tests
+{
+    [TestClass]
+    public class MyFirstUnitTests
+    {
+        [TestMethod]
+        public void MyFirstUselessTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void CalculatorAdd_TwoPositiveNumbers()
+        {
+            Calculator calc = new Calculator();
+
+            calc.Add(2, 2);
+
+            Assert.AreEqual(4, calc.Result);
+        }
+    }
+}
---
+++ MyFirstUnitTestTutorial/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstUnitTestTutorial
+{
+    public class Calculator
+    {
+        public int Result { get; set; } = 0;
+        public Calculator()
+        {
+               
+        }
+
+        public void Add(int x, int y)
+        {
+            Result += x + y;
+        }
+    }
+}
